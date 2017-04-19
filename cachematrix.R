39ec44d4b56ee09e879365ff8952b3ea7d64cf21
NeetikaKapoor <nksehdev@rms.com>
2017-04-19 12:03:06 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache inverse of a matrix so it need not be calculated repeatedly
 
-## Write a short comment describing this function
+## Create a matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## Compute inverse once else retrieve from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("cached data available")
+    return(inv)
+  }
+  my_mat <- x$get()
+  inv <- solve(my_mat, ...)
+  x$setInverse(inv)
+  inv
 }
